c7a78cbacc722d4de781164bf1e16ac087b84922
Matt Martin <matt92martin@gmail.com>
2016-11-02 14:42:10 -0700

    More work on popup

--- src/js/TimeLines.js
+++ src/js/TimeLines.js
@@ -1,11 +1,77 @@
-(function(){
+$(function(){
+    var list = $('#list').html();
+    var add = $('#add').html();
 
-	function TellMeItsReal(e){
-		console.log("It's real");
-		console.log(e);
-	}
+    var listTemplate = Handlebars.compile(list);
+    var addTemplate = Handlebars.compile(add);
 
-	var TellMe = document.querySelector('#TellMeItsReal');
-	TellMe.onclick = TellMeItsReal;
+    var main = $('#main');
+
+    function getListData(){
+        return {title: "My New Post", body: "This is my first post!"};
+        // chrome.storage.local.get({
+        //     portal: data.portal,
+        //     projectname: data.projectname,
+        //     team: data.team,
+        //     type: data.type,
+        //     hours: data.hours,
+        //     complexity: data.complexity,
+        //     noq: data.noq,
+        //     programmername: data.programmername,
+        //     state: data.state,
+        //     rdate: data.rdate,
+        //     ddate: data.ddate,
+        // }, null);
+    }
+
+    function renderList(){
+
+        var context = getListData();
+        main.html(listTemplate(context));
+
+        $('#add-list-item').on('click', function(){
+            renderAdd();
+        });
+    }
+
+    function renderAdd(){
+
+        var context = {test: 'Anything'};
+        main.html(addTemplate(context));
+
+        $('#form').on('submit', function(e){
+            var form = $(this).serializeArray();
+            var data = {};
+            form.map(function(d){
+                data[d.name] = d.value;
+            });
+            setNew(data);
+            renderList();
+            e.preventDefault()
+        });
+    }
+
+
+
+    function setNew(data) {
+
+        // chrome.storage.local.set({
+        //     portal: data.portal,
+        //     projectname: data.projectname,
+        //     team: data.team,
+        //     type: data.type,
+        //     hours: data.hours,
+        //     complexity: data.complexity,
+        //     noq: data.noq,
+        //     programmername: data.programmername,
+        //     state: data.state,
+        //     rdate: data.rdate,
+        //     ddate: data.ddate,
+        // }, null);
+    }
+
+
+    renderList();
+
 
 });
