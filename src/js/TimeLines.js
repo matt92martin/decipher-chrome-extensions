6c4c26f312baa2d3d001d68a92f2d1756e831b86
Matt Martin <matt92martin@gmail.com>
2016-11-03 11:34:14 -0700

    Getting portal info

--- src/js/TimeLines.js
+++ src/js/TimeLines.js
@@ -7,8 +7,15 @@ $(function(){
 
     var main = $('#main');
 
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    }
+
     function getListData(){
-        return {title: "My New Post", body: "This is my first post!"};
+        // return {title: "My New Post", body: "This is my first post!"};
         // chrome.storage.local.get({
         //     portal: data.portal,
         //     projectname: data.projectname,
@@ -38,6 +45,34 @@ $(function(){
 
         var context = {test: 'Anything'};
         main.html(addTemplate(context));
+
+        chrome.storage.sync.get({
+            name: '',
+            team: ''
+        }, function(items){
+
+            chrome.tabs.query({'active': true, 'lastFocusedWindow': true},
+                function(tabs) {
+
+                    var url = tabs[0].url;
+
+                    if (url.indexOf('decipherinc') != -1){
+
+                        var path = url.split('selfserve')[1];
+                        var server = url.split('.decipherinc')[0].split(/http(?:s):\/\//)[1];
+
+                        var portal = `https://${server}.decipherinc.com/apps/portal/#/projects/detail/selfserve${path}`;
+                        document.getElementsByName('portal')[0].value = portal ? portal : '';
+                    }
+                });
+
+            var today = new Date();
+
+            document.getElementsByName('uniqueID')[0].value = guid();
+            document.getElementsByName('rdate')[0].value = today.getMonth() + "-" + today.getDate() + "-" + today.getFullYear();
+            document.getElementsByName('programmername')[0].value = items.name;
+            document.getElementsByName('team')[0].value = items.team;
+        });
 
         $('#form').on('submit', function(e){
             var form = $(this).serializeArray();
